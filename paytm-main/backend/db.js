877882d2111233ee94a47d33de38262bb724ab71
Sudhanshu Ranjan <rsudhanshu620@gmail.com>
2024-06-12 16:12:16 +0530

    paytm project starting here

--- paytm-main/backend/db.js
+++ paytm-main/backend/db.js
@@ -1,3 +1,3 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://Paytm:paytm123@sudhanshu-ranjan.imrttev.mongodb.net/")
+mongoose.connect("mongodb+srv://Paytm:paytm123@sudhanshu-ranjan.imrttev.mongodb.net/");
