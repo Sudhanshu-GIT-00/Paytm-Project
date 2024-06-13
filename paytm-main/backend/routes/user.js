e1debfe5bbb6aaeaff1e737ca975f1d334f653d2
Sudhanshu Ranjan <rsudhanshu620@gmail.com>
2024-06-13 15:48:16 +0530

    some change

--- paytm-main/backend/routes/user.js
+++ paytm-main/backend/routes/user.js
@@ -1,42 +1,42 @@
 const express = require("express");
 const zod = require("zod");
-const {User} = require("../db");
+const { User } = require("../db");
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = require("../config");
 const router = express.Router();
 
 // singup and singin Routes
 const singupSchema = zod.object({
-    username:zod.string(),
-    password:zod.string(),
-    firstName:zod.string(),
-    lastName:zod.string()
+    username: zod.string(),
+    password: zod.string(),
+    firstName: zod.string(),
+    lastName: zod.string()
 })
 
-router.post("/singup",async(req, res) => {
+router.post("/singup", async (req, res) => {
     const body = req.body;
-    const {success}=singupSchema.safeParse(req.body);
-    if(!success){
+    const obj = singupSchema.safeParse(req.body);
+    if (!obj.success) {
         return res.json({
-            message:"Email already taken / Incorrect inputs"
+            message: "Email already taken / Incorrect inputs"
         })
     }
-    const user =user.Router.findOne({
-        username:body.username
+    const user = user.Router.findOne({
+        username: body.username
     })
 
-    if(user._id){
+    if (user._id) {
         return res.json({
-            message:"Email already taken / Incorrect inputs"
+            message: "Email already taken / Incorrect inputs"
         })
     }
     const dbUser = await User.create(body);
     const token = jwt.sing({
-        userId:dbUser._id
+        userId: dbUser._id
     }, JWT_SECRET)
 
     res.json({
-        message:"User Created Successfully",
+        message: "User Created Successfully",
         token: token
     })
 })
