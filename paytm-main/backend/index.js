d766fba1425e9136ced53fddc4f9fc4676faf24f
Sudhanshu Ranjan <rsudhanshu620@gmail.com>
2024-06-24 01:13:05 +0530

    added acount file

--- paytm-main/backend/index.js
+++ paytm-main/backend/index.js
@@ -1,14 +1,15 @@
-const express = require("express");
+// backend/index.js
+const express = require('express');
 const cors = require("cors");
+const rootRouter = require("./routes/index");
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const mainRouter = require("./routes/index");
+app.use("/api/v1", rootRouter);
 
-const app = express();
-
-app.use("api/v1", mainRouter);
 app.listen(3000);
 
 // /api/v1/user/signup
